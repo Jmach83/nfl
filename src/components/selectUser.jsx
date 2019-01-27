0ff85dfbcf8a8371f3f0933815215a5dda4fd086
Jmach <jmach83@gmail.com>
2019-01-27 12:26:17 +0100

    Add fake data and launch.json for debug

---
+++ src/components/selectUser.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SelectUser extends Component {
+    state = {  }
+    render() { 
+        return ( 
+           <h1>Select user</h1>
+         );
+    }
+}
+ 
+export default SelectUser;
